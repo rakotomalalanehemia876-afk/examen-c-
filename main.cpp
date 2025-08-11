710abc452ee832ccd7932a18ac68c86d672f9c8b
Rakotomalala Nehemia <rakotomalalanehemia876@gmail.com>
2025-08-11 10:47:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string chaine;
+    cout << "Entrez une chaine en CamelCase : "<< endl;
+    cin >> chaine;
+
+    string resultat;
+    resultat += chaine[0];
+
+    for(int i = 1; i < chaine.size(); i++) {
+
+        if(chaine[i] >= 'A' && chaine[i] <= 'Z') {
+            resultat += " ";
+        }
+        resultat += chaine[i];
+    }
+
+    cout << "Phrase convertie : " << resultat << endl;
+    return 0;
+}
